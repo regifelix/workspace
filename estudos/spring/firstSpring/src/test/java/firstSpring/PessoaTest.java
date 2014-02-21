71a65ee9720e146d391f5352ce2ce413c10e0741
Reginaldo Donizete Felix <regifelix@gmail.com>
2014-02-21 17:52:42 -0300

    Alteracoes de 21-02-2014

--- estudos/spring/firstSpring/src/test/java/firstSpring/PessoaTest.java
+++ estudos/spring/firstSpring/src/test/java/firstSpring/PessoaTest.java
@@ -3,13 +3,14 @@ package firstSpring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = { "file:WebContent/WEB-INF/persistencia.xml" })
-//@Transactional
+@ContextConfiguration(locations = { "classpath:spring-context.xml" })
+
 @TransactionConfiguration(defaultRollback = false)
 public class PessoaTest {
 	//@Autowired
